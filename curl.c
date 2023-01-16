68523bd4bd32f72e6bfbcb0243e2d09196002474
pena <lpena@myges.fr>
2023-01-16 21:02:17 +0100

    FIN

--- curl.c
+++ curl.c
@@ -7,16 +7,17 @@ int main(void)
   CURLcode res;
 
   curl = curl_easy_init();
+
   if(curl) {
     curl_easy_setopt(curl, CURLOPT_URL, "https://ges-cas.kordis.fr/login");
     res = curl_easy_perform(curl);
-    // Check for errors 
+
     if(res != CURLE_OK)
       fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
+      
+      curl_easy_strerror(res));
 
-    // always cleanup 
-    curl_easy_cleanup(curl);
+      curl_easy_cleanup(curl);
   }
   return 0;
 }
